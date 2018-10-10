b6823c792c56415d556411a3c50138890f7f21e9
ICE <ice@lab05m38.nrc.ice.ufjf.br>
2018-10-10 19:49:29 -0300

    Criação da classe evento

---
+++ app/src/main/java/a20183/dcc192/trab1joaopcassio/Evento.java
@@ -0,0 +1,57 @@
+package a20183.dcc192.trab1joaopcassio;
+
+import java.util.Date;
+
+public class Evento {
+     String título,  facilitador , descrição;
+     Date data;
+     Date hora;
+
+    public String getTítulo() {
+        return título;
+    }
+
+    public void setTítulo(String título) {
+        this.título = título;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public String getFacilitador() {
+        return facilitador;
+    }
+
+    public void setFacilitador(String facilitador) {
+        this.facilitador = facilitador;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    public Evento(String título, String facilitador, String descrição, Date data, Date hora) {
+        this.título = título;
+        this.facilitador = facilitador;
+        this.descrição = descrição;
+        this.data = data;
+        this.hora = hora;
+    }
+}
