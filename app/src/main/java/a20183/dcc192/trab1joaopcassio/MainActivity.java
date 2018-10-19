db5b188aca0d7e08e7e19468f53c0e8ea597361f
JoaoPSJC <jpcxm@hotmail.com>
2018-10-19 10:57:43 -0300

    rclViews do main iniciados

--- app/src/main/java/a20183/dcc192/trab1joaopcassio/MainActivity.java
+++ app/src/main/java/a20183/dcc192/trab1joaopcassio/MainActivity.java
@@ -3,6 +3,8 @@ package a20183.dcc192.trab1joaopcassio;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,14 +12,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int CADS_EVENTO = 0, DET_EVENTO = 1, INSC_EVENTO = 2, CADS_PARTICIPANTE = 3, DET_PARTICIPANTE = 4;
     private Button btnCadsParticipante, btnCadsEvento;
+    private RecyclerView rclParticipantes, rclEventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCadsParticipante = (Button) findViewById(R.id.btn_cadsParticipante);
-        btnCadsEvento = (Button) findViewById(R.id.btn_cadsEvento);
+        btnCadsParticipante = (Button) findViewById(R.id.btn_cadsParticipanteMain);
+        btnCadsEvento = (Button) findViewById(R.id.btn_cadsEventoMain);
+
+        rclParticipantes = (RecyclerView) findViewById(R.id.rlc_participantesMain);
+        rclParticipantes.setLayoutManager(new LinearLayoutManager(this));
+        rclEventos = (RecyclerView) findViewById(R.id.rlc_eventoMain);
+        rclEventos.setLayoutManager(new LinearLayoutManager(this));
+
+        
 
         btnCadsParticipante.setOnClickListener(new View.OnClickListener() {
             @Override
