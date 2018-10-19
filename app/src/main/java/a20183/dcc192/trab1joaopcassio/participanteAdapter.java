ba47b1d55923237025743763020eced94b78af3c
JoaoPSJC <jpcxm@hotmail.com>
2018-10-19 15:25:56 -0300

    adapter participante feito

--- app/src/main/java/a20183/dcc192/trab1joaopcassio/participanteAdapter.java
+++ app/src/main/java/a20183/dcc192/trab1joaopcassio/participanteAdapter.java
@@ -1,9 +1,12 @@
 package a20183.dcc192.trab1joaopcassio;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -21,24 +24,30 @@ class participanteAdapter extends RecyclerView.Adapter<participanteAdapter.ViewH
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View participanteView =inflater.inflate(R.layout.rcl_layout, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(participanteView);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Participante participante = participantes.get(position);
+        viewHolder.nome.setText(participante.getNome());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return participantes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
-
+        public TextView nome;
         public ViewHolder(final View itemView) {
             super(itemView);
+            nome = itemView.findViewById(R.id.txt_nomeTitulo);
         }
     }
 
